0b94a01b290913fee335829c26323577e16b478f
povser75@outlook.com <povser75@outlook.com>
2024-09-13 19:52:24 +0400

    Добавьте файлы проекта.

---
+++ Bookingcom/HotelForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Bookingcom
+{
+    partial class HotelForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HotelPanel = new System.Windows.Forms.Panel();
+            this.RoomsPanel = new System.Windows.Forms.Panel();
+            this.HotelPictureBox = new System.Windows.Forms.PictureBox();
+            this.HotelLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.HotelPanel.SuspendLayout();
+            this.RoomsPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.HotelPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // HotelPanel
+            // 
+            this.HotelPanel.Controls.Add(this.HotelLabel);
+            this.HotelPanel.Controls.Add(this.HotelPictureBox);
+            this.HotelPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.HotelPanel.Location = new System.Drawing.Point(0, 0);
+            this.HotelPanel.Name = "HotelPanel";
+            this.HotelPanel.Size = new System.Drawing.Size(1094, 293);
+            this.HotelPanel.TabIndex = 0;
+            // 
+            // RoomsPanel
+            // 
+            this.RoomsPanel.Controls.Add(this.label1);
+            this.RoomsPanel.Controls.Add(this.pictureBox1);
+            this.RoomsPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.RoomsPanel.Location = new System.Drawing.Point(0, 293);
+            this.RoomsPanel.Name = "RoomsPanel";
+            this.RoomsPanel.Size = new System.Drawing.Size(1094, 362);
+            this.RoomsPanel.TabIndex = 1;
+            // 
+            // HotelPictureBox
+            // 
+            this.HotelPictureBox.Location = new System.Drawing.Point(20, 17);
+            this.HotelPictureBox.Name = "HotelPictureBox";
+            this.HotelPictureBox.Size = new System.Drawing.Size(366, 214);
+            this.HotelPictureBox.TabIndex = 0;
+            this.HotelPictureBox.TabStop = false;
+            // 
+            // HotelLabel
+            // 
+            this.HotelLabel.AutoSize = true;
+            this.HotelLabel.Location = new System.Drawing.Point(16, 247);
+            this.HotelLabel.Name = "HotelLabel";
+            this.HotelLabel.Size = new System.Drawing.Size(58, 22);
+            this.HotelLabel.TabIndex = 1;
+            this.HotelLabel.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(24, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(361, 245);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 294);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // HotelForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1094, 655);
+            this.Controls.Add(this.RoomsPanel);
+            this.Controls.Add(this.HotelPanel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "HotelForm";
+            this.Text = "HotelForm";
+            this.HotelPanel.ResumeLayout(false);
+            this.HotelPanel.PerformLayout();
+            this.RoomsPanel.ResumeLayout(false);
+            this.RoomsPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.HotelPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel HotelPanel;
+        private System.Windows.Forms.Label HotelLabel;
+        private System.Windows.Forms.PictureBox HotelPictureBox;
+        private System.Windows.Forms.Panel RoomsPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ Bookingcom/MainForm.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace Bookingcom
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.AuthPanel = new System.Windows.Forms.Panel();
+            this.AuthBtn = new System.Windows.Forms.Button();
+            this.PasTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LoginTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.FiltrPanel = new System.Windows.Forms.Panel();
+            this.FindButton = new System.Windows.Forms.Button();
+            this.RatingComboBox = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.CityComboBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.AuthPanel.SuspendLayout();
+            this.FiltrPanel.SuspendLayout();
+            this.MainPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AuthPanel
+            // 
+            this.AuthPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.AuthPanel.Controls.Add(this.AuthBtn);
+            this.AuthPanel.Controls.Add(this.PasTextBox);
+            this.AuthPanel.Controls.Add(this.label2);
+            this.AuthPanel.Controls.Add(this.LoginTextBox);
+            this.AuthPanel.Controls.Add(this.label1);
+            this.AuthPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.AuthPanel.Location = new System.Drawing.Point(0, 0);
+            this.AuthPanel.Name = "AuthPanel";
+            this.AuthPanel.Size = new System.Drawing.Size(1261, 86);
+            this.AuthPanel.TabIndex = 0;
+            // 
+            // AuthBtn
+            // 
+            this.AuthBtn.Location = new System.Drawing.Point(599, 26);
+            this.AuthBtn.Name = "AuthBtn";
+            this.AuthBtn.Size = new System.Drawing.Size(150, 31);
+            this.AuthBtn.TabIndex = 4;
+            this.AuthBtn.Text = "Войти";
+            this.AuthBtn.UseVisualStyleBackColor = true;
+            // 
+            // PasTextBox
+            // 
+            this.PasTextBox.Location = new System.Drawing.Point(383, 26);
+            this.PasTextBox.Name = "PasTextBox";
+            this.PasTextBox.Size = new System.Drawing.Size(198, 28);
+            this.PasTextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(300, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Пароль:";
+            // 
+            // LoginTextBox
+            // 
+            this.LoginTextBox.Location = new System.Drawing.Point(83, 26);
+            this.LoginTextBox.Name = "LoginTextBox";
+            this.LoginTextBox.Size = new System.Drawing.Size(198, 28);
+            this.LoginTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Логин:";
+            // 
+            // FiltrPanel
+            // 
+            this.FiltrPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.FiltrPanel.Controls.Add(this.FindButton);
+            this.FiltrPanel.Controls.Add(this.RatingComboBox);
+            this.FiltrPanel.Controls.Add(this.label4);
+            this.FiltrPanel.Controls.Add(this.CityComboBox);
+            this.FiltrPanel.Controls.Add(this.label3);
+            this.FiltrPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.FiltrPanel.Location = new System.Drawing.Point(0, 86);
+            this.FiltrPanel.Name = "FiltrPanel";
+            this.FiltrPanel.Size = new System.Drawing.Size(1261, 78);
+            this.FiltrPanel.TabIndex = 1;
+            // 
+            // FindButton
+            // 
+            this.FindButton.Location = new System.Drawing.Point(1074, 6);
+            this.FindButton.Name = "FindButton";
+            this.FindButton.Size = new System.Drawing.Size(175, 64);
+            this.FindButton.TabIndex = 4;
+            this.FindButton.Text = "Найти";
+            this.FindButton.UseVisualStyleBackColor = true;
+            // 
+            // RatingComboBox
+            // 
+            this.RatingComboBox.FormattingEnabled = true;
+            this.RatingComboBox.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.RatingComboBox.Location = new System.Drawing.Point(365, 24);
+            this.RatingComboBox.Name = "RatingComboBox";
+            this.RatingComboBox.Size = new System.Drawing.Size(168, 30);
+            this.RatingComboBox.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(282, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 22);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Рейтинг";
+            // 
+            // CityComboBox
+            // 
+            this.CityComboBox.FormattingEnabled = true;
+            this.CityComboBox.Location = new System.Drawing.Point(96, 24);
+            this.CityComboBox.Name = "CityComboBox";
+            this.CityComboBox.Size = new System.Drawing.Size(168, 30);
+            this.CityComboBox.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 22);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Город";
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.BackColor = System.Drawing.Color.White;
+            this.MainPanel.Controls.Add(this.label6);
+            this.MainPanel.Controls.Add(this.pictureBox2);
+            this.MainPanel.Controls.Add(this.label5);
+            this.MainPanel.Controls.Add(this.pictureBox1);
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel.Location = new System.Drawing.Point(0, 164);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(1261, 489);
+            this.MainPanel.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(436, 331);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(164, 22);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Гостиница \"Волга\"";
+            this.label6.Click += new System.EventHandler(this.HotelLabel_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(440, 61);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(359, 256);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.HotelPB_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(54, 331);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(168, 22);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Гостиница \"Венец\"";
+            this.label5.Click += new System.EventHandler(this.HotelLabel_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(58, 61);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(359, 256);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.HotelPB_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1261, 653);
+            this.Controls.Add(this.MainPanel);
+            this.Controls.Add(this.FiltrPanel);
+            this.Controls.Add(this.AuthPanel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainForm";
+            this.Text = "Bookigcom";
+            this.AuthPanel.ResumeLayout(false);
+            this.AuthPanel.PerformLayout();
+            this.FiltrPanel.ResumeLayout(false);
+            this.FiltrPanel.PerformLayout();
+            this.MainPanel.ResumeLayout(false);
+            this.MainPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel AuthPanel;
+        private System.Windows.Forms.Button AuthBtn;
+        private System.Windows.Forms.TextBox PasTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox LoginTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel FiltrPanel;
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.Button FindButton;
+        private System.Windows.Forms.ComboBox RatingComboBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox CityComboBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
