5ce369af6636ba264ac3d97ea5702b87797bbc8d
povser75@outlook.com <povser75@outlook.com>
2024-09-16 19:56:41 +0400

    Макет готов

--- Bookingcom/HotelForm.Designer.cs
+++ Bookingcom/HotelForm.Designer.cs
@@ -28,32 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HotelForm));
             this.HotelPanel = new System.Windows.Forms.Panel();
             this.HotelLabel = new System.Windows.Forms.Label();
             this.HotelPictureBox = new System.Windows.Forms.PictureBox();
             this.RoomsPanel = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.HotelDescriptionTB = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.AdresLabel = new System.Windows.Forms.Label();
+            this.TelLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.HotelPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.HotelPictureBox)).BeginInit();
             this.RoomsPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // HotelPanel
             // 
+            this.HotelPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.HotelPanel.Controls.Add(this.TelLabel);
+            this.HotelPanel.Controls.Add(this.label4);
+            this.HotelPanel.Controls.Add(this.AdresLabel);
+            this.HotelPanel.Controls.Add(this.label2);
+            this.HotelPanel.Controls.Add(this.pictureBox2);
+            this.HotelPanel.Controls.Add(this.HotelDescriptionTB);
             this.HotelPanel.Controls.Add(this.HotelLabel);
             this.HotelPanel.Controls.Add(this.HotelPictureBox);
             this.HotelPanel.Dock = System.Windows.Forms.DockStyle.Top;
             this.HotelPanel.Location = new System.Drawing.Point(0, 0);
             this.HotelPanel.Name = "HotelPanel";
-            this.HotelPanel.Size = new System.Drawing.Size(1094, 293);
+            this.HotelPanel.Size = new System.Drawing.Size(1094, 320);
             this.HotelPanel.TabIndex = 0;
             // 
             // HotelLabel
             // 
             this.HotelLabel.AutoSize = true;
-            this.HotelLabel.Location = new System.Drawing.Point(16, 247);
+            this.HotelLabel.Location = new System.Drawing.Point(16, 272);
             this.HotelLabel.Name = "HotelLabel";
             this.HotelLabel.Size = new System.Drawing.Size(58, 22);
             this.HotelLabel.TabIndex = 1;
@@ -63,19 +78,20 @@
             // 
             this.HotelPictureBox.Location = new System.Drawing.Point(20, 17);
             this.HotelPictureBox.Name = "HotelPictureBox";
-            this.HotelPictureBox.Size = new System.Drawing.Size(366, 214);
+            this.HotelPictureBox.Size = new System.Drawing.Size(366, 252);
             this.HotelPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.HotelPictureBox.TabIndex = 0;
             this.HotelPictureBox.TabStop = false;
             // 
             // RoomsPanel
             // 
+            this.RoomsPanel.BackColor = System.Drawing.Color.White;
             this.RoomsPanel.Controls.Add(this.label1);
             this.RoomsPanel.Controls.Add(this.pictureBox1);
             this.RoomsPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.RoomsPanel.Location = new System.Drawing.Point(0, 293);
+            this.RoomsPanel.Location = new System.Drawing.Point(0, 320);
             this.RoomsPanel.Name = "RoomsPanel";
-            this.RoomsPanel.Size = new System.Drawing.Size(1094, 362);
+            this.RoomsPanel.Size = new System.Drawing.Size(1094, 335);
             this.RoomsPanel.TabIndex = 1;
             // 
             // label1
@@ -96,6 +112,62 @@
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
+            // HotelDescriptionTB
+            // 
+            this.HotelDescriptionTB.Enabled = false;
+            this.HotelDescriptionTB.Location = new System.Drawing.Point(428, 114);
+            this.HotelDescriptionTB.Multiline = true;
+            this.HotelDescriptionTB.Name = "HotelDescriptionTB";
+            this.HotelDescriptionTB.ReadOnly = true;
+            this.HotelDescriptionTB.Size = new System.Drawing.Size(625, 155);
+            this.HotelDescriptionTB.TabIndex = 2;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(428, 17);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(48, 42);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(684, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 22);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Адрес:";
+            // 
+            // AdresLabel
+            // 
+            this.AdresLabel.AutoSize = true;
+            this.AdresLabel.Location = new System.Drawing.Point(757, 57);
+            this.AdresLabel.Name = "AdresLabel";
+            this.AdresLabel.Size = new System.Drawing.Size(30, 22);
+            this.AdresLabel.TabIndex = 5;
+            this.AdresLabel.Text = "    ";
+            // 
+            // TelLabel
+            // 
+            this.TelLabel.AutoSize = true;
+            this.TelLabel.Location = new System.Drawing.Point(757, 89);
+            this.TelLabel.Name = "TelLabel";
+            this.TelLabel.Size = new System.Drawing.Size(30, 22);
+            this.TelLabel.TabIndex = 7;
+            this.TelLabel.Text = "    ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(684, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 22);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Тел.:";
+            // 
             // HotelForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
@@ -113,6 +185,7 @@
             this.RoomsPanel.ResumeLayout(false);
             this.RoomsPanel.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -125,5 +198,11 @@
         private System.Windows.Forms.Panel RoomsPanel;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label TelLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label AdresLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox HotelDescriptionTB;
     }
 }
--- Bookingcom/RoomForm.Designer.cs
+++ Bookingcom/RoomForm.Designer.cs
@@ -31,12 +31,22 @@
             this.RoomPanel = new System.Windows.Forms.Panel();
             this.RoomPictureBox = new System.Windows.Forms.PictureBox();
             this.RoomLabel = new System.Windows.Forms.Label();
+            this.RoomDescriptionTB = new System.Windows.Forms.TextBox();
+            this.BookingPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.IndateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.OutdateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.BookingButton = new System.Windows.Forms.Button();
             this.RoomPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.RoomPictureBox)).BeginInit();
+            this.BookingPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // RoomPanel
             // 
+            this.RoomPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.RoomPanel.Controls.Add(this.RoomDescriptionTB);
             this.RoomPanel.Controls.Add(this.RoomLabel);
             this.RoomPanel.Controls.Add(this.RoomPictureBox);
             this.RoomPanel.Dock = System.Windows.Forms.DockStyle.Top;
@@ -62,11 +72,77 @@
             this.RoomLabel.TabIndex = 1;
             this.RoomLabel.Text = "label1";
             // 
+            // RoomDescriptionTB
+            // 
+            this.RoomDescriptionTB.Enabled = false;
+            this.RoomDescriptionTB.Location = new System.Drawing.Point(382, 31);
+            this.RoomDescriptionTB.Multiline = true;
+            this.RoomDescriptionTB.Name = "RoomDescriptionTB";
+            this.RoomDescriptionTB.ReadOnly = true;
+            this.RoomDescriptionTB.Size = new System.Drawing.Size(644, 124);
+            this.RoomDescriptionTB.TabIndex = 2;
+            // 
+            // BookingPanel
+            // 
+            this.BookingPanel.BackColor = System.Drawing.Color.White;
+            this.BookingPanel.Controls.Add(this.BookingButton);
+            this.BookingPanel.Controls.Add(this.OutdateTimePicker);
+            this.BookingPanel.Controls.Add(this.IndateTimePicker);
+            this.BookingPanel.Controls.Add(this.label2);
+            this.BookingPanel.Controls.Add(this.label1);
+            this.BookingPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BookingPanel.Location = new System.Drawing.Point(0, 330);
+            this.BookingPanel.Name = "BookingPanel";
+            this.BookingPanel.Size = new System.Drawing.Size(1053, 150);
+            this.BookingPanel.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(231, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Дата заезда:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(231, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Дата выезда:";
+            // 
+            // IndateTimePicker
+            // 
+            this.IndateTimePicker.Location = new System.Drawing.Point(381, 36);
+            this.IndateTimePicker.Name = "IndateTimePicker";
+            this.IndateTimePicker.Size = new System.Drawing.Size(200, 28);
+            this.IndateTimePicker.TabIndex = 2;
+            // 
+            // OutdateTimePicker
+            // 
+            this.OutdateTimePicker.Location = new System.Drawing.Point(381, 74);
+            this.OutdateTimePicker.Name = "OutdateTimePicker";
+            this.OutdateTimePicker.Size = new System.Drawing.Size(200, 28);
+            this.OutdateTimePicker.TabIndex = 3;
+            // 
+            // BookingButton
+            // 
+            this.BookingButton.Location = new System.Drawing.Point(606, 36);
+            this.BookingButton.Name = "BookingButton";
+            this.BookingButton.Size = new System.Drawing.Size(235, 65);
+            this.BookingButton.TabIndex = 4;
+            this.BookingButton.Text = "Забронировать";
+            this.BookingButton.UseVisualStyleBackColor = true;
+            // 
             // RoomForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1053, 629);
+            this.ClientSize = new System.Drawing.Size(1053, 480);
+            this.Controls.Add(this.BookingPanel);
             this.Controls.Add(this.RoomPanel);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
@@ -75,6 +151,8 @@
             this.RoomPanel.ResumeLayout(false);
             this.RoomPanel.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.RoomPictureBox)).EndInit();
+            this.BookingPanel.ResumeLayout(false);
+            this.BookingPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -84,5 +162,12 @@
         private System.Windows.Forms.Panel RoomPanel;
         private System.Windows.Forms.Label RoomLabel;
         private System.Windows.Forms.PictureBox RoomPictureBox;
+        private System.Windows.Forms.TextBox RoomDescriptionTB;
+        private System.Windows.Forms.Panel BookingPanel;
+        private System.Windows.Forms.Button BookingButton;
+        private System.Windows.Forms.DateTimePicker OutdateTimePicker;
+        private System.Windows.Forms.DateTimePicker IndateTimePicker;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
