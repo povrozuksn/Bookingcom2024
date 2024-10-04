69b30b3160b0fbb2446a25e1693fd81e1f28c94d
povser75@outlook.com <povser75@outlook.com>
2024-10-04 19:47:47 +0400

    Подключение к БД

--- Bookingcom/MainForm.cs
+++ Bookingcom/MainForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Common;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,11 +16,36 @@ namespace Bookingcom
     public partial class MainForm : Form
     {
         const string CONNECTION_STRING =
-            "SslMode=none; Server=localhost; Database=booking2; potr=3306; Uid=root;";
+            "SslMode=none; Server=localhost; Database=booking2; port=3306; Uid=root;";
+
+        public static List<string> cities = new List<string>();
 
         public MainForm()
         {
             InitializeComponent();
+
+            MySqlConnection conn = new MySqlConnection(CONNECTION_STRING);
+            conn.Open();
+
+            MySqlCommand cmd = new MySqlCommand("SELECT id, Name FROM cities", conn);
+            DbDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                string id = reader.GetString(0);
+                string name = reader.GetString(1);
+
+                cities.Add(id);
+                cities.Add(name);
+            }
+            reader.Close();
+            conn.Close();
+
+            for(int i=0; i<cities.Count; i+=2)
+            {
+                CityComboBox.Items.Add(cities[i] + ". "+ cities[i+1]);
+
+            }
+
             FiltrPanel.Height = FiltrButton.Height;
         }
 
