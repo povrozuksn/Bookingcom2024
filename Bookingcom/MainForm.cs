8e9681dfa4504ca4fcd1138eb7cc19b1714475cb
povser75@outlook.com <povser75@outlook.com>
2024-10-07 20:08:45 +0400

    Твблица Нotels в БД, функция SELECT запроса, SQLClass

--- Bookingcom/MainForm.cs
+++ Bookingcom/MainForm.cs
@@ -14,38 +14,21 @@ using MySql.Data.MySqlClient;
 namespace Bookingcom
 {
     public partial class MainForm : Form
-    {
-        const string CONNECTION_STRING =
-            "SslMode=none; Server=localhost; Database=booking2; port=3306; Uid=root;";
-
-        public static List<string> cities = new List<string>();
-
+    { 
         public MainForm()
         {
             InitializeComponent();
 
-            MySqlConnection conn = new MySqlConnection(CONNECTION_STRING);
-            conn.Open();
-
-            MySqlCommand cmd = new MySqlCommand("SELECT id, Name FROM cities", conn);
-            DbDataReader reader = cmd.ExecuteReader();
-            while (reader.Read())
-            {
-                string id = reader.GetString(0);
-                string name = reader.GetString(1);
-
-                cities.Add(id);
-                cities.Add(name);
-            }
-            reader.Close();
-            conn.Close();
+            List<string> cities = SQLClass.MySelect("SELECT id, Name FROM cities");
+            List<string> hotels = SQLClass.MySelect("SELECT id, name, rating, id_city FROM hotels");
+          
 
             for(int i=0; i<cities.Count; i+=2)
             {
                 CityComboBox.Items.Add(cities[i] + ". "+ cities[i+1]);
-
             }
 
+           
             FiltrPanel.Height = FiltrButton.Height;
         }
 
