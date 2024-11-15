fd3b840e08c8866ea422a17ff507502c35d76fb2
povser75@outlook.com <povser75@outlook.com>
2024-11-15 20:00:03 +0400

    Добавление  и удаление гостиниц

--- Bookingcom/AdminHotelsForm.Designer.cs
+++ Bookingcom/AdminHotelsForm.Designer.cs
@@ -29,18 +29,24 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.NameHotelTextBox = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.CityComboBox = new System.Windows.Forms.ComboBox();
-            this.AddHotelBtn = new System.Windows.Forms.Button();
-            this.PicHotelPictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.RatingComboBox = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
             this.AddPicHotelBtn = new System.Windows.Forms.Button();
+            this.PicHotelPictureBox1 = new System.Windows.Forms.PictureBox();
+            this.AddHotelBtn = new System.Windows.Forms.Button();
+            this.CityComboBox = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.NameHotelTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -49,6 +55,7 @@
             // 
             // panel1
             // 
+            this.panel1.AutoScroll = true;
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
@@ -59,9 +66,42 @@
             this.panel1.Size = new System.Drawing.Size(718, 299);
             this.panel1.TabIndex = 0;
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(350, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Город";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(100, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(228, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Наименование гостиницы";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "id";
+            // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.panel2.Controls.Add(this.label11);
+            this.panel2.Controls.Add(this.label10);
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.RatingComboBox);
+            this.panel2.Controls.Add(this.label7);
             this.panel2.Controls.Add(this.AddPicHotelBtn);
             this.panel2.Controls.Add(this.PicHotelPictureBox1);
             this.panel2.Controls.Add(this.AddHotelBtn);
@@ -76,83 +116,77 @@
             this.panel2.Size = new System.Drawing.Size(718, 300);
             this.panel2.TabIndex = 1;
             // 
-            // label1
+            // label11
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(15, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = " №п/п";
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(45, 271);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(331, 20);
+            this.label11.TabIndex = 13;
+            this.label11.Text = "- поля, обязательный для заполнения";
             // 
-            // label2
+            // label10
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(100, 20);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(228, 20);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Наименование гостиницы";
+            this.label10.AutoSize = true;
+            this.label10.ForeColor = System.Drawing.Color.Red;
+            this.label10.Location = new System.Drawing.Point(32, 271);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(15, 20);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "*";
             // 
-            // label3
+            // label9
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(350, 20);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(61, 20);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Город";
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.Color.Red;
+            this.label9.Location = new System.Drawing.Point(88, 143);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(15, 20);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "*";
             // 
-            // label4
+            // label8
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(3, 3);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(206, 20);
-            this.label4.TabIndex = 0;
-            this.label4.Text = "Добавление гостиницы";
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(116, 28);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(15, 20);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "*";
             // 
-            // label5
+            // RatingComboBox
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(30, 48);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(91, 20);
-            this.label5.TabIndex = 1;
-            this.label5.Text = "Название";
+            this.RatingComboBox.FormattingEnabled = true;
+            this.RatingComboBox.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.RatingComboBox.Location = new System.Drawing.Point(36, 110);
+            this.RatingComboBox.Name = "RatingComboBox";
+            this.RatingComboBox.Size = new System.Drawing.Size(289, 28);
+            this.RatingComboBox.TabIndex = 9;
             // 
-            // NameHotelTextBox
+            // label7
             // 
-            this.NameHotelTextBox.Location = new System.Drawing.Point(34, 71);
-            this.NameHotelTextBox.Name = "NameHotelTextBox";
-            this.NameHotelTextBox.Size = new System.Drawing.Size(291, 27);
-            this.NameHotelTextBox.TabIndex = 2;
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(31, 85);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 20);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Рейтинг";
             // 
-            // label6
+            // AddPicHotelBtn
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(30, 111);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(61, 20);
-            this.label6.TabIndex = 3;
-            this.label6.Text = "Город";
-            // 
-            // CityComboBox
-            // 
-            this.CityComboBox.FormattingEnabled = true;
-            this.CityComboBox.Location = new System.Drawing.Point(35, 136);
-            this.CityComboBox.Name = "CityComboBox";
-            this.CityComboBox.Size = new System.Drawing.Size(289, 28);
-            this.CityComboBox.TabIndex = 4;
-            // 
-            // AddHotelBtn
-            // 
-            this.AddHotelBtn.Location = new System.Drawing.Point(34, 223);
-            this.AddHotelBtn.Name = "AddHotelBtn";
-            this.AddHotelBtn.Size = new System.Drawing.Size(157, 40);
-            this.AddHotelBtn.TabIndex = 5;
-            this.AddHotelBtn.Text = "Добавить";
-            this.AddHotelBtn.UseVisualStyleBackColor = true;
+            this.AddPicHotelBtn.Location = new System.Drawing.Point(368, 36);
+            this.AddPicHotelBtn.Name = "AddPicHotelBtn";
+            this.AddPicHotelBtn.Size = new System.Drawing.Size(316, 29);
+            this.AddPicHotelBtn.TabIndex = 7;
+            this.AddPicHotelBtn.Text = "Добавить картинку";
+            this.AddPicHotelBtn.UseVisualStyleBackColor = true;
+            this.AddPicHotelBtn.Click += new System.EventHandler(this.AddPicHotelBtn_Click);
             // 
             // PicHotelPictureBox1
             // 
@@ -164,15 +198,57 @@
             this.PicHotelPictureBox1.TabIndex = 6;
             this.PicHotelPictureBox1.TabStop = false;
             // 
-            // AddPicHotelBtn
+            // AddHotelBtn
             // 
-            this.AddPicHotelBtn.Location = new System.Drawing.Point(368, 36);
-            this.AddPicHotelBtn.Name = "AddPicHotelBtn";
-            this.AddPicHotelBtn.Size = new System.Drawing.Size(316, 29);
-            this.AddPicHotelBtn.TabIndex = 7;
-            this.AddPicHotelBtn.Text = "Добавить картинку";
-            this.AddPicHotelBtn.UseVisualStyleBackColor = true;
-            this.AddPicHotelBtn.Click += new System.EventHandler(this.AddPicHotelBtn_Click);
+            this.AddHotelBtn.Location = new System.Drawing.Point(34, 223);
+            this.AddHotelBtn.Name = "AddHotelBtn";
+            this.AddHotelBtn.Size = new System.Drawing.Size(157, 40);
+            this.AddHotelBtn.TabIndex = 5;
+            this.AddHotelBtn.Text = "Добавить";
+            this.AddHotelBtn.UseVisualStyleBackColor = true;
+            this.AddHotelBtn.Click += new System.EventHandler(this.AddHotelBtn_Click);
+            // 
+            // CityComboBox
+            // 
+            this.CityComboBox.FormattingEnabled = true;
+            this.CityComboBox.Location = new System.Drawing.Point(36, 166);
+            this.CityComboBox.Name = "CityComboBox";
+            this.CityComboBox.Size = new System.Drawing.Size(289, 28);
+            this.CityComboBox.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(31, 141);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 20);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Город";
+            // 
+            // NameHotelTextBox
+            // 
+            this.NameHotelTextBox.Location = new System.Drawing.Point(34, 52);
+            this.NameHotelTextBox.Name = "NameHotelTextBox";
+            this.NameHotelTextBox.Size = new System.Drawing.Size(291, 27);
+            this.NameHotelTextBox.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 29);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 20);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Название";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 3);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(206, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Добавление гостиницы";
             // 
             // openFileDialog1
             // 
@@ -186,7 +262,7 @@
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "AdminHotelsForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "AdminHotelsForm";
@@ -216,5 +292,11 @@
         private System.Windows.Forms.TextBox NameHotelTextBox;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ComboBox RatingComboBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
     }
 }
--- Bookingcom/AdminHotelsForm.cs
+++ Bookingcom/AdminHotelsForm.cs
@@ -20,6 +20,11 @@ namespace Bookingcom
 
         private void AdminHotelsForm_Load(object sender, EventArgs e)
         {
+            panel1.Controls.Clear();
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label3);
+
             List<string> cities = SQLClass.MySelect("SELECT id, Name FROM cities");
 
             CityComboBox.Items.Clear();
@@ -27,7 +32,6 @@ namespace Bookingcom
             {
                 CityComboBox.Items.Add(cities[i] + ". " + cities[i + 1]);
             }
-
 
             List<string> hotels = SQLClass.MySelect("SELECT id, name, id_city FROM hotels");
 
@@ -38,6 +42,7 @@ namespace Bookingcom
                 lbl1.Text = hotels[i];
                 lbl1.Location = new Point(20, y);
                 lbl1.Size = new Size(50, 30);
+                lbl1.Tag = hotels[i];
                 panel1.Controls.Add(lbl1);
 
                 Label lbl2 = new Label();
@@ -54,6 +59,13 @@ namespace Bookingcom
                 lbl3.Size = new Size(200, 30);
                 panel1.Controls.Add(lbl3);
 
+                Button btn = new Button();
+                btn.Location = new Point(550, y);
+                btn.Size = new Size(100, 30);
+                btn.Text = "Удалить";
+                btn.Click += new EventHandler(DelHotelClick);
+                panel1.Controls.Add(btn);
+
                 y += 40;
             }
         }
@@ -68,5 +80,39 @@ namespace Bookingcom
                 adress = Path.GetFileName(adress);
             }
         }
+
+        private void AddHotelBtn_Click(object sender, EventArgs e)
+        {
+            if(NameHotelTextBox.Text != "" && CityComboBox.Text != "")
+            {
+                SQLClass.MyUpDate("INSERT INTO hotels (name, rating, id_city, image) VALUES ('" + NameHotelTextBox.Text + "','" + RatingComboBox.Text + "','" + CityComboBox.Text + "','" + adress +"' )");
+                MessageBox.Show("Сохранено");
+                NameHotelTextBox.Text = "";
+                RatingComboBox.Text = "";
+                CityComboBox.Text = "";
+                PicHotelPictureBox1.Image = null;
+                AdminHotelsForm_Load(sender, e);
+            }
+            else
+            {
+                MessageBox.Show("Заполните обязательный поля");
+            }
+        }
+
+        private void DelHotelClick(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            int y = btn.Location.Y;
+            foreach (Control control in panel1.Controls)
+            {
+                if (control.Location == new Point(20, y))
+                {
+                    SQLClass.MyUpDate("DELETE FROM hotels WHERE id = '" + control.Tag + "'");
+                    MessageBox.Show("Успешно удалено");
+                }
+            }
+            AdminHotelsForm_Load(sender, e);
+        }
+
     }
 }
