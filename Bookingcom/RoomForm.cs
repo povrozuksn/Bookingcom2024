9c8da3999a33507f283545a19479c530cab93403
povser75@outlook.com <povser75@outlook.com>
2024-12-09 19:35:27 +0400

    Бронирование номеров с защитой

--- Bookingcom/RoomForm.cs
+++ Bookingcom/RoomForm.cs
@@ -52,6 +52,18 @@ namespace Bookingcom
                 MessageBox.Show("Вы не авторизованы");
                 return;
             }
+            DateTime dt = IndateTimePicker.Value;
+
+            while(dt <= OutdateTimePicker.Value.AddDays(0.5))
+            {
+                List<string> exitBooking = SQLClass.MySelect("SELECT COUNT(*) FROM booking WHERE dateFrom <= '" + dt.ToString("yyyy-MM-dd") + "' AND dateTo >= '" + dt.ToString("yyyy-MM-dd") + "' AND room_id = " + idRoom);
+                if (Convert.ToInt32(exitBooking[0]) > 0)
+                {
+                    MessageBox.Show("На эти даты номер забронирован. Выберите другую дату.");
+                    return;
+                }
+                dt = dt.AddDays(1);
+            }
             SQLClass.MyUpDate("INSERT INTO booking (user, dateFrom, dateTo, room_id) VALUES ('" + MainForm.Login + "', '" + IndateTimePicker.Value.ToString("yyyy-MM-dd") + "', '" + OutdateTimePicker.Value.ToString("yyyy-MM-dd") + "', '" + idRoom + "')");
             MessageBox.Show("Бронирование прошло успешно");
         }
